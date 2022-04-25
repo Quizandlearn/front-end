694f90beaf1a60123dcbb584e046305efc0605c3
sbevilacqua <sbevilacqua@omnilog.fr>
2022-04-25 22:22:50 +0200

    fix- URL route Explore

--- src/app/App.js
+++ src/app/App.js
@@ -8,6 +8,10 @@ import Explore from "../pages/Explore/Explore";
 import MyProfile from "../pages/Profile/MyProfile";
 import Login from "../pages/Login/Login";
 import Signup from "../pages/Signup/Signup";
+import Categories from '../pages/Categories/Categories';
+import MyQuizzes from '../pages/MyQuizzes/MyQuizzes';
+import FavoriteQuizzes from '../pages/FavoriteQuizzes/FavoriteQuizzes';
+import CompletedQuizzes from '../pages/CompletedQuizzes/CompletedQuizzes';
 
 const App = () => {
   return (
@@ -20,9 +24,13 @@ const App = () => {
           <Route path="signup" element={<Signup />} />
         </Route>
 
-        <Route element={<RequireAuth />}>
-          <Route path="/explore" element={<Explore />} />
+       <Route element={<RequireAuth />}>
+          <Route path="/quizzes" element={<Explore />} />
           <Route path="/myProfile" element={<MyProfile />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/myQuizzes" element={<MyQuizzes />} />
+          <Route path="/favoriteQuizzes" element={<FavoriteQuizzes />} />
+          <Route path="/completedQuizzes" element={<CompletedQuizzes />} />
         </Route>
 
         {/* route qui n'existe pas - redirection page Explore*/}
