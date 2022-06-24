57873ba2b186c9e49d4d46dfb081409801c0c684
CamilleLafrance <cam.lafr@free.fr>
2022-06-24 17:54:37 +0200

    details for each item

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,24 +1,57 @@
 import React from "react";
 import useAuth from "../../hooks/useAuth";
 import "./Explore.css";
+import profile from "../../assets/profile.png";
 
 const Explore = () => {
   // eslint-disable-next-line no-unused-vars
   const { user } = useAuth();
+
+  const quizzes = [{
+    titleQuiz: "Ceci est un très très très long titre de quiz...",
+    category: "CATÉGORIE",
+    author: "Lisa",
+    mark: "★★★★☆",
+  }, {
+    titleQuiz: "Ceci est un autre très très long titre de quiz...",
+    category: "CATEGORY",
+    author: "Antoine",
+    mark: "★★★☆☆",
+  },
+  ];
+
+  const exploreTitle = "Les derniers quiz ajoutés";
+  const descriptionQuiz = "En savoir plus";
+
   return (
     <div className="explore">
-      <h2 className="explore__title">Les derniers quiz ajoutés</h2>
+      <h2 className="explore__title">{exploreTitle}</h2>
       <ul>
-        <li>
-          <div className="explore__list__item">
-            <h3 className="explore__list__item-title">Ceci est un très long long titre de quiz...</h3>
-            <div className="explore__list__item__details">
-              <div className="explore__list__item__details-category">#Catégorie</div>
-              <div className="explore__list__item__details-stars">★★★★☆</div>
+        {quizzes && quizzes.map((quiz) => (
+          <li key={quiz.titleQuiz}>
+            <div className="explore__list__item">
+              <div className="explore__list__item__text">
+                <h3 className="explore__list__item__text__title">{quiz.titleQuiz}</h3>
+                <div className="explore__list__item__text__details">
+                  <div className="explore__list__item__text__details-category">{quiz.category}</div>
+                  <div className="explore__list__item__text__details-stars">{quiz.mark}</div>
+                </div>
+                <div className="explore__list__item__text__description">{descriptionQuiz}</div>
+              </div>
+              <div className="profile-img">
+                <figure className="image is-64x64">
+                  <img
+                    src={profile}
+                    alt="Profile"
+                  />
+                </figure>
+                par
+                {" "}
+                {quiz.author}
+              </div>
             </div>
-            <div className="explore__list__item-description">En savoir plus</div>
-          </div>
-        </li>
+          </li>
+        ))}
       </ul>
     </div>
   );
