64e09b308100f437c4d64923a24cbbc62b4c9c66
Sara Bevilacqua <sarabev6@gmail.com>
2022-10-07 12:32:48 +0200

    feat-answer-quiz-header

--- src/pages/AnswerQuiz/AnswerQuiz.jsx
+++ src/pages/AnswerQuiz/AnswerQuiz.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import "./AnswerQuiz.css";
+import profileQuiz from "../../assets/profileQuiz.png";
 // import useGetQuizzes from "../../hooks/useGetQuizzes";
 /* eslint-disable arrow-body-style */
 
@@ -11,7 +13,23 @@ const AnswerQuiz = () => {
   const { from } = location.state;
   console.log("location", from);
   return (
-    <div>Quiz</div>
+    <main className="answerQuiz">
+      <div className="answerQuiz__block">
+        <h1 className="answerQuiz__block__title">Answer Quiz title</h1>
+        <div className="answerQuiz__block__information">
+          <h2 className="answerQuiz__block__information__title">Questionnaire React</h2>
+          <div className="answerQuiz__block__information__title__userName">
+            <p>Quiz créé par Marion Lenoir</p>
+            <figure className="image is-64x64">
+              <img
+                src={profileQuiz}
+                alt="Profile"
+              />
+            </figure>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 };
 
