992cb0e34f7fa392d25275217e83623001ed6ca0
Camille LAFRANCE <77630883+CamilleLafrance@users.noreply.github.com>
2023-02-27 16:59:21 +0100

    Create page MyQuizzes (static) (#64)
    * create page myquizzes
    
    * case no quizzes
    
    * improve case no quizzes
    
    * add eslint rule
    
    * remove eslint rule

--- src/pages/MyQuizzes/MyQuizzes.jsx
+++ src/pages/MyQuizzes/MyQuizzes.jsx
@@ -1,10 +1,57 @@
 import React from "react";
-/* eslint-disable arrow-body-style */
+import useGetMyQuizzes from "../../hooks/useGetMyQuizzes";
+import Loading from "../../assets/Loading";
+import "../Explore/Explore.css";
+import "./MyQuizzes.css";
 
 const MyQuizzes = () => {
+  const { data, loading } = useGetMyQuizzes();
+  const exploreTitle = "Mes quiz";
+  const descriptionQuiz = "En savoir plus";
+  const ratingsQuiz = "★★★★☆";
+  const noQuizzes = "...";
+
   return (
-    <main>Page Mes Quiz!</main>
+    <>
+      {loading && <Loading />}
+      {!loading && data && (
+        <main className="explore">
+          <h2 className="explore__title">{exploreTitle}</h2>
+          {data &&
+            data.map((quiz) => (
+              <div className="explore__list__item" key={quiz._id}>
+                <div className="explore__list__item__text">
+                  <h3 className="explore__list__item__text__title">
+                    {quiz.title}
+                  </h3>
+                  <div className="explore__list__item__text__stars">
+                    {ratingsQuiz}
+                  </div>
+                  {" "}
+                  <button
+                    className="explore__list__item__text__category"
+                    type="button"
+                    href="https://quiz-and-learn-heroku-front.herokuapp.com/"
+                  >
+                    {quiz.categories}
+                  </button>
+                  <p
+                    title={quiz.description}
+                    className="explore__list__item__text__description"
+                  >
+                    {descriptionQuiz}
+                  </p>
+                </div>
+              </div>
+            ))}
+        </main>
+      )}
+      {!loading && (
+        <main className="explore">
+          <div className="myQuizzes__noQuizzes">{noQuizzes}</div>
+        </main>
+      )}
+    </>
   );
 };
-
 export default MyQuizzes;
